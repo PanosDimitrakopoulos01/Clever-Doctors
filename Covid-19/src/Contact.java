84a880b854b59c24dc6927ece1eaf9ac6af0f313
PanosDimitrakopoulos01 <PanosDimitrakopoulos01@users.noreply.github.com>
2020-11-23 10:38:47 +0200

    Update Contact.java

--- Covid-19/src/Contact.java
+++ Covid-19/src/Contact.java
@@ -1,27 +1,19 @@
 
 public class Contact {
 
-	private String firstName;
-	private String lastName;
+	private String fullName
 	private String email;
 	
-	public Contact(String firstName, String lastName, String email) {
-		this.firstName = firstName;
-		this.lastName = lastName;
+	public Contact(String fullName, String email) {
+		this.fullName = fullName;
 		this.email = email;
 	}
 	
-	public String getFirstName() {
-		return firstName;
+	public String getFullName() {
+		return fullName;
 	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
 	}
 	public String getEmail() {
 		return email;
