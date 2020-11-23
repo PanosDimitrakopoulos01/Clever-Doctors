f332574e0ddf6dad1ab3675e803859c24ebcce86
PanosDimitrakopoulos01 <PanosDimitrakopoulos01@users.noreply.github.com>
2020-11-23 11:12:58 +0200

    ...

--- Covid-19/src/TestCovid19.java
+++ Covid-19/src/TestCovid19.java
@@ -14,34 +14,7 @@ public class TestCovid19 {
 		
  
 
-		    do
-			{
-				System.out.println("ÅéóÜãåôå ôá óôïé÷åßá ôïõ áóèåíÞ!");
-				
-				System.out.println("ÅéóÜãåôå ôo üíïìá ôïõ áóèåíÞ:");
-				do {
-					String fullName = in.nextLine();
-					flag = check.validation(0, fullName);
-					if(!flag) {
-						System.out.println("Ìç Ýãêõñç åéóáãùãÞ. ÅéóÜãåôå îáíÜ ôo üíïìá ôïõ áóèåíÞ:");
-					}
-				} while(!flag);
-				
-				System.out.println("ÅéóÜãåôå ôo ôçëÝöùíï ôïõ áóèåíÞ:");
-				do {
-					String phone = in.nextLine();
-					flag = check.validation(3, phone);
-					if(!flag) {
-						System.out.println("Ìç Ýãêõñç åéóáãùãÞ. ÅéóÜãåôå îáíÜ ôo ôçëÝöùíï ôïõ áóèåíÞ:");
-					}
-				} while(!flag);
-		
-				
-				
-				
-				
-				
-			} while (true);
+		   
 
 
 		
