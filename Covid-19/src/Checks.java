8ef299aca1e9e386f75a3f645539f5fd511173c2
EleftheriaGk <72806121+EleftheriaGk@users.noreply.github.com>
2020-11-30 21:00:44 +0200

    Update Checks.java

--- Covid-19/src/Checks.java
+++ Covid-19/src/Checks.java
@@ -3,7 +3,7 @@ import java.util.regex.*;
 public class Checks {
 	
 	private static String nameRegex = "[Á-Ù]{3,} [Á-Ù]{3,}";
-	//private static String dateRegex = "^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$";
+	private static String dateRegex = "[1-31]"+ "/"+"[1-12]";
 	private static String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+"[a-zA-Z0-9_+&*-]+)*@"+"(?:[a-zA-Z0-9-]+\\.)+[a-z" + "A-Z]{2,7}$"; 
 	private static String phoneRegex = "[0-9]{10,10}";
 
@@ -14,9 +14,9 @@ public class Checks {
 		case 0:
 			regX = nameRegex;
 			break;
-		//case 1:
-			//regX = dateRegex;
-			//break;
+		case 1:
+			regX = dateRegex;
+			break;
 		case 2:
 			regX = emailRegex;
 			break;
