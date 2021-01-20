79bb5a55cf477661a520091741a81ab3f44b9dbc
ediamantopoulou <72871392+ediamantopoulou@users.noreply.github.com>
2021-01-20 21:32:31 +0200

    Update MainJava.java

--- MainJava.java
+++ MainJava.java
@@ -4,8 +4,8 @@ public class MainJava {
 	    public static void main(String[] args){
 	    	JFrame frame= new JFrame();
 	        frame.setTitle("Welecome to Clever Doctors App");
+		frame.setVisible(true);
 	        frame.setSize(600, 400);
-	        frame.setVisible(true);
 	        graphicsjava DC = new graphicsjava();
 	        frame.add(DC);
 	        
